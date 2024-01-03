ed8a0e32ea9c87319ce57bcb1a87f24e48a89324
Bryan1604 <luongvuhs@gmail.com>
2024-01-03 09:59:46 +0700

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Luong Vu on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
