f618890b5b7a707357fc3c7e541f00d76e88fc36
luongvd <luongvuhs@gmail.com>
2024-01-30 15:52:58 +0700

    add search view

--- Airbnb/Core/Explore/DestinationSearchView.swift
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -7,18 +7,133 @@
 
 import SwiftUI
 
+enum DestinationSearchOption {
+    case location
+    case dates
+    case guests
+}
+
 struct DestinationSearchView: View {
     @Binding var show: Bool
+    @State private var destination = ""
+    
+    @State private var selectedOption: DestinationSearchOption = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numGuest = 0
     var body: some View {
-        VStack{
-            Button {
-                withAnimation(.snappy){
-                    show.toggle()
+        VStack {
+            HStack {
+                Button {
+                    withAnimation(.snappy) {
+                        show.toggle()
+                    }
+                }label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
                 }
-            }label: {
-                Image(systemName: "xmark.circle")
-                    .imageScale(.large)
-                    .foregroundStyle(.black)
+                
+                Spacer()
+                
+                if !destination.isEmpty {
+                    Button("Clear") {
+                        destination = ""
+                    }
+                    .foregroundStyle(.red)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+            }
+            .padding()
+            
+            VStack(alignment: .leading) {
+                if(selectedOption == .location) {
+                    Text("Where to go?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search destinations", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.0)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsedPickerView(title: "Where", description: "Add destinations")
+                }
+                
+            }
+            .modifier(CollapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .location ? 120  : 64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .location
+                }
+            }
+            
+            //date selection view
+            VStack {
+                if(selectedOption == .dates) {
+                    HStack {
+                        Text("When's your trip")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    VStack {
+                        DatePicker("From", selection: $startDate , displayedComponents: .date )
+                        Divider()
+                        DatePicker("To", selection: $endDate , displayedComponents: .date)
+                    }
+                    .foregroundStyle(.gray)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                } else {
+                    CollapsedPickerView(title: "When", description: "Add dates")
+                }
+            }
+            .modifier(CollapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .dates ? 180  : 64)
+            .onTapGesture {
+                 
+                withAnimation(.snappy) {
+                    selectedOption = .dates
+                }
+            }
+           
+            // num guest view
+            VStack {
+                if(selectedOption == .guests) {
+                    HStack {
+                        Text("Who's comming")
+                        Spacer()
+                    }
+                    Stepper {
+                        Text("\(numGuest) Adults")
+                    } onIncrement: {
+                        numGuest += 1
+                    } onDecrement: {
+                        guard numGuest > 0 else { return }
+                        numGuest -= 1
+                    }
+                    
+                } else {
+                    CollapsedPickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .modifier(CollapsibleDestinationViewModifier())
+            .frame(height: selectedOption == .guests ? 120  : 64)
+            .onTapGesture {
+                withAnimation(.snappy) {
+                    selectedOption = .guests
+                }
             }
         }
     }
@@ -26,4 +141,33 @@ struct DestinationSearchView: View {
 
 #Preview {
     DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsibleDestinationViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+    }
+}
+
+struct CollapsedPickerView: View {
+    let title: String
+    let description: String
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                Text(description  )
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+        }
+    }
 }
